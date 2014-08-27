cb283ec45731dc280e88950976180858c8f41045
matthewpoletin <contact@matthewpoletin.ru>
2014-08-27 18:27:27 +0400

    Время начинать

---
+++ straminata/straminata/input.cpp
@@ -0,0 +1,15 @@
+#include "input.h"
+#include <iostream>
+
+input::input()
+{
+	std::cout << " tick\n";
+}
+input::~input()
+{
+	std::cout << "Bye!";
+}
+int input::handleInput()
+{
+	
+}
---
+++ straminata/straminata/input.h
@@ -0,0 +1,11 @@
+class input
+{
+public:
+	bool InputState;
+private:
+	input();
+	virtual ~input();
+
+	int input::handleInput();
+};
+
