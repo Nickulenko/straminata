7e3e9a75ed3fe9cf40e2ff0dc2f1c77094e1be28
matthewpoletin <contact@matthewpoletin.ru>
2014-09-07 12:54:29 +0400

    Консоль
    Добавил отключение консоли по выбору.

--- straminata/straminata/main.cpp
+++ straminata/straminata/main.cpp
@@ -5,26 +5,23 @@
 
 int main(int argc, const char** argv)
 {
+	// Main ingame settings
 	const int m_width = 800;
 	const int m_height = 600;
 	const char* m_title = "Straminata"; // game title
 	const bool m_windowType = 0; // 0 - windowed, 1 - fullscreen
+	const bool m_debugMode = 1; // 0 - release, 1 - debug
 
 	Display display(m_width, m_height, m_title, m_windowType); //create a window
+
+	if (!m_debugMode) ShowWindow(GetConsoleWindow(), SW_HIDE);
 
 	while (!display.IsClosed())
 	{
 		display.Clear(1.0f, 1.0f, 1.0f, 1.0f);
 
-		glMatrixMode(GL_PROJECTION); // ставим текущей матрицу проектирования
-		glLoadIdentity(); // загрузка единичной матрицы
-
-		glMatrixMode(GL_MODELVIEW); // ставим текущей модельную матрицу
-		glLoadIdentity(); // загружаем единичную матрицу
-
 		glBegin(GL_QUADS); // рисуем четырехугольник
 		glColor3f(1.0f, 0.0f, 0.0f); // установка цвета рисования
-		// задания позиции вектора фигуры
 			glVertex3f(0.0f, 0.0f, -4.0f);
 			glColor3f(0.0f, 1.0f, 0.0f);
 			glVertex3f(-1.0f, -2.0f, -4.0f);
