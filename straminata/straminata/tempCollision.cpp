919b208c86e975d1b99d4a392a69d092efe8064b
matthewpoletin <contact@matthewpoletin.ru>
2014-09-26 19:21:37 +0400

    temporary collision method

---
+++ straminata/straminata/tempCollision.cpp
@@ -0,0 +1,17 @@
+// Collision map
+
+//int objWidth = 40;
+//int objHeight = 50;
+//
+//bool Collide(int objWidth, int objHeight)
+//{
+//	bool collision = false;
+//	// Go through all cells and check for object there
+//	for (int counter = y - objHeight; counter < objHeight + 20; counter += 10)
+//		if (map[x + 20][i])
+//		{
+//		collision = true;
+//		break;
+//		}
+//	return collision;
+//}
