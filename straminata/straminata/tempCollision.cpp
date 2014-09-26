acbbb9f858f8a541d719a2de9d20d2b44404b55a
Nickulenko <cincout@mail.ru>
2014-09-26 19:19:05 +0300

    Update tempCollision.cpp

--- straminata/straminata/tempCollision.cpp
+++ straminata/straminata/tempCollision.cpp
@@ -5,10 +5,11 @@
 //
 //bool Collide(int objWidth, int objHeight)
 //{
+//  Перемещение вправо accelerationx>0
 //	bool collision = false;
 //	// Go through all cells and check for object there
-//	for (int counter = y - objHeight; counter < objHeight + 20; counter += 10)
-//		if (map[x + 20][i])
+//	for (int counter = y - objHeight/2; counter < y + objHeight/2; counter += 10)//половина надо, так как x,y совпадает с серединой
+//		if (map[x + objWidth/2][i])
 //		{
 //		collision = true;
 //		break;
